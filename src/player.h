220aae6612cc1040b7d4627d22afe77844585e74
Ciprian Stanciu <stanciu.gabriel.ciprian@gmail.com>
2024-04-10 22:58:09 +0200

    try acceleration on position

--- src/player.h
+++ src/player.h
@@ -19,6 +19,37 @@ public:
     glm::vec3 forward { 0, 0, -1 };
     glm::vec3 up { 0, 1, 0 };
 
+    struct Movement {
+        float speed{ 0.0f };
+        float accel{ 0.0003f };
+        float decel{ 0.0005f };
+
+        void update(int dir, float frametime) {
+            if (dir == -1) {
+                if (speed > 0.0f)
+                    speed -= (accel + decel) * frametime;
+                else
+                    speed -= accel * frametime;
+            }
+            else if (dir == 1) {
+                if (speed < 0.0f)
+                    speed += (accel + decel) * frametime;
+                else
+                    speed += accel * frametime;
+            }
+            else {
+                if (speed != 0.0f) {
+                    float prevSpeed = speed;
+                    speed -= decel * frametime * (speed > 0.0f ? 1 : -1);
+                    if (prevSpeed * speed < 0.0f)
+                        speed = 0.0f;
+                }
+            }
+        }
+    };
+
+    Movement moveRight, moveForward, moveUp;
+
 private:
     const Window* m_pWindow;
     static constexpr glm::vec3 s_yAxis { 0, 1, 0 };
