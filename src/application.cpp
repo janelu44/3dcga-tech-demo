508ae98e2770e382ea9d59e3f1c234259457eae4
Ciprian Stanciu <stanciu.gabriel.ciprian@gmail.com>
2024-04-10 15:54:48 +0200

    planeta ioan (placeholder name)

--- src/application.cpp
+++ src/application.cpp
@@ -22,6 +22,7 @@ DISABLE_WARNINGS_PUSH()
 #include <glm/gtx/quaternion.hpp>
 #include <glm/mat4x4.hpp>
 #include <imgui/imgui.h>
+#include "bezier/path.h"
 
 DISABLE_WARNINGS_POP()
 
@@ -239,6 +240,10 @@ public:
 
         moon.revolutionProgress += moon.revolutionSpeed;
         moon.orbitProgress += moon.orbitSpeed;
+
+        ioan.revolutionProgress += ioan.revolutionSpeed;
+        ioan.orbitProgress += ioan.orbitSpeed;
+        ioan.orbitProgress = (ioan.orbitProgress >= 1.0f) ? ioan.orbitProgress - 1.0f : ioan.orbitProgress;
     }
 
     void renderSolarSystem(const Shader& shader, glm::mat4 mvpMatrix, bool renderSun = true) {
@@ -297,6 +302,21 @@ public:
 
             glUniformMatrix4fv(1, 1, GL_FALSE, glm::value_ptr(moonScale));
             glUniformMatrix3fv(2, 1, GL_FALSE, glm::value_ptr(moonNormal));
+            glUniform1i(4, GL_FALSE);
+            glUniform3fv(7, 1, glm::value_ptr(glm::vec3(0.7f, 0.7f, 0.7f)));
+            glUniform1i(8, GL_FALSE);
+
+            // ioan
+            glm::mat4 ioanPos = sunPos;
+            ioanPos = glm::translate(ioanPos, ioanPath.evaluate(ioan.orbitProgress));
+            ioanPos = glm::translate(ioanPos, glm::vec3(ioan.orbitSize, 0, 0));
+            glm::mat4 ioanRot = glm::rotate(ioanPos, glm::radians(ioan.revolutionProgress),
+                glm::vec3(0, 1, 0));
+            glm::mat4 ioanNormal = glm::inverseTranspose(glm::mat3(ioanRot));
+            glm::mat4 ioanScale = glm::scale(ioanRot, glm::vec3(ioan.size));
+
+            glUniformMatrix4fv(1, 1, GL_FALSE, glm::value_ptr(ioanScale));
+            glUniformMatrix3fv(2, 1, GL_FALSE, glm::value_ptr(ioanNormal));
             glUniform1i(4, GL_FALSE);
             glUniform3fv(7, 1, glm::value_ptr(glm::vec3(0.7f, 0.7f, 0.7f)));
             glUniform1i(8, GL_FALSE);
@@ -553,6 +573,16 @@ private:
     Planet sun{ 3.0f, 0.0f, 0.0f, 0.0f, 0.1f, 0.0f };
     Planet earth{ 1.0f, 7.0f, 0.0f, 0.0f, 1.0f, 0.3f };
     Planet moon{ 0.2f, 2.0f, 0.0f, 0.0f, 0.0f, 0.5f };
+    Planet ioan{ 1.0f, 7.0f, 0.0f, 0.0f, 1.0f, 0.005f };
+
+    BezierPath ioanPath{
+        {{1.0f, 0.0f, -1.0f}, {2.0f, 0.0f, -1.0f}, {2.0f, 0.0f, 1.0f}, {1.0f, 0.0f, 1.0f}},
+        {
+            {{-1.0f, 0.0f, -1.0f}, {0.0f, 0.0f, -1.0f}},
+            {{-1.0f, 0.0f, 1.0f}, {-2.0f, 0.0f, 1.0f}},
+        },
+        true
+    };
 };
 
 int main() {
